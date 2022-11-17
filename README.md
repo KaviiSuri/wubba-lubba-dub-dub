c30980d1295ba247de3cb5729d6a9cd576d83528
Kavii Suri <kavii@Kaviis-MacBook-Pro.local>
2022-11-18 01:32:50 +0530

    removed react-query-devtools and added README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wubba Lubba Dub Dub!
+
+This is a simple search app that queries the [Rick and Morty API](https://rickandmortyapi.com/) and renders the output in an infinite scrolling list.
+
+## Features
+- Infinite Scrolling
+- Debouncing of Queries
+- Handling Pagination
+- Responsive
+
+## Tools Used
+- **Build Tool** - Vite
+- **Frontend Framework** - ReactJS
+- **Data Fetching** - Fetch API + React Query
+- **Debouncing** - useDebounce hook
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -12,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </React.StrictMode>
 )
