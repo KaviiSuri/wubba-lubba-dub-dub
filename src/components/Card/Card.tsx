41f9e487d520920caa6c20feb98bf6a7436f008c
Kavii Suri <kavii@Kaviis-MacBook-Pro.local>
2022-11-18 01:08:57 +0530

    completed UI and responsiveness

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import styles from './styles.modules.css'
+import { Character, CharacterStatus } from '../../services/rickAndMorty'
+import styles from './styles.module.css'
 
-const Card = () => {
+
+
+const classForStatus: Record<CharacterStatus, string> = {
+  Alive: styles.alive,
+  Dead: styles.dead,
+  Unknown: styles.unknown,
+}
+const CharacterStatusLabel = ({ status }: { status: CharacterStatus }) => {
+
+  return <span className={`${styles.statusLabel} ${classForStatus[status]}`}> {status}</span>
+}
+
+const Card = ({ character }: { character: Character }) => {
   return (
-    <div className={styles.cardsContainer}>Card</div>
+    <div className={styles.cardsContainer}>
+      <img src={character.image} />
+      <div className={styles.desc}>
+        <h3>{character.name}</h3>
+        <p> <CharacterStatusLabel status={character.status} /> - {character.species}</p>
+        <p><span className={styles.label}>First Seen At: </span> {character.origin.name}</p>
+        <p><span className={styles.label}>Last Seen At: </span> {character.location.name}</p>
+      </div>
+    </div>
   )
 }
 
--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { CharactersResponse } from '../../services/rickAndMorty'
+import Card from '../Card/Card'
 import styles from './styles.module.css'
 
 
@@ -9,10 +10,10 @@ type CharacterListProps = {
 const CharacterList = ({ response }: CharacterListProps) => {
   return (
     <div className={styles.resultsContainer}>
-      <h3>{response.info.count}</h3>
-      <pre>
-        {JSON.stringify(response.results.map(res => ({name: res.name, id: res.id})), null, 2)}
-      </pre>
+      {response.results.map(character => <Card character={character} key={character.id} />)}
+      {/* <pre> */}
+      {/*   {JSON.stringify(response.results.map(res => ({name: res.name, id: res.id})), null, 2)} */}
+      {/* </pre> */}
     </div>
   )
 }
